183704d3eecdc4637c42185c57a48ee3e6fee1e8
Eric <eric_minei@yahoo.com>
2025-01-17 00:19:58 -0300

    Acrecenta o POST no front

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import {
   Admin,
   Resource,
   List,
-  EditGuesser,
-  ShowGuesser,
   Datagrid,
-  TextField
+  TextField,
+  Create,
+  SimpleForm,
+  TextInput,
+  BooleanField,
+  BooleanInput
 } from "react-admin";
 import { Layout } from "./Layout";
 import restProvider from 'ra-data-simple-rest'
@@ -17,18 +20,45 @@ export const App = () => (
     authProvider={authProvider} 
     dataProvider={restProvider('http://localhost:8000')}
   >
-    <Resource name="users" list={UserList}/>
+    <Resource 
+      name="users" 
+      list={UserList}
+      create={UserCreate}
+    />
   </Admin>
 );
 
 const UserList = (props: any) => {
+
+
   return (
   <List {...props}>
-    <Datagrid>
-      <TextField source='id' />
-      <TextField source='title' />
+    <Datagrid>  
+      <TextField source='phone_number' />
+      <TextField source='first_name' />
+      <TextField source='last_name' />
+      <BooleanField source="ads_enabled" />
+      <BooleanField source="is_active" />
     </Datagrid>
   </List>
   )
 
 }
+
+const UserCreate = (props: any) => {
+
+  const postDefaultValue = () => ({ updated_at: new Date() });
+
+  return (
+    <Create title='Create a User' {...props}>
+      <SimpleForm defaultValues={postDefaultValue}>
+        <TextInput source='id' />
+        <TextInput source='first_name' />
+        <TextInput source='last_name' />
+        <BooleanInput source="ads_enabled" />
+        <BooleanInput source="is_active" />
+      </SimpleForm>
+    </Create>
+  )
+}
+
